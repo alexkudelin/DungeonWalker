f1dbf59eecfdd3601de1f8ab995919279520b659
Aleksei Kudelin <aleksei.enable@gmail.com>
2021-06-28 00:27:07 +0300

    draft

--- Scripts/UI/CoinGui.gd
+++ Scripts/UI/CoinGui.gd
@@ -2,8 +2,14 @@ extends Area2D
 
 
 func OnCoinAreaEntered(body):
-	print(body.get_name())
-	print("coin in")
+#	var world = get_parent().get_parent().get_parent()
+#	var pos = tilemap.world_to_map(body.position)
+#	tilemap.set_cellv(pos, -1)
+	if get_parent().visible:
+#		if body.get_name() == "Player":
+		get_parent().visible = false
+
+		print("coin in")
 
 
 func OnCoinAreaExited(_body):
